e7885ace67dc5e1b86f90ac3a8f7703b9fecc744
someone <macavalcantep@gmail>
2017-03-15 20:25:11 -0300

    Solange

--- src/main/java/br/unip/sd/DeadLock.java
+++ src/main/java/br/unip/sd/DeadLock.java
@@ -1,17 +1,26 @@
-package br.unip.sd;
+//package br.unip.sd;
 
 /**
  * Created by dirceu on 14/03/17.
  */
+
 public class DeadLock {
     static class Amigo {
+    	private final String frase;
         private final String nome;
-        public Amigo(String nome) {
+        public Amigo(String nome, String frase) {
             this.nome = nome;
+            this.frase = frase;
         }
+        
         public String getNome() {
             return this.nome;
         }
+
+        public String getFrase() {
+            return this.frase;
+        }
+        
         public synchronized void passar(Amigo passador) {
             System.out.printf("%s: %s  me passou a bola!%n",
                     new Object[]{this.nome, passador.getNome()});
@@ -25,15 +34,21 @@ public class DeadLock {
     }
 
     public static void main(String[] args) {
+    	final Amigo frase =
+                new Amigo("", "Quase não foi"); 
+        StringBuffer sb = new StringBuffer(frase.getFrase());
+        sb.reverse();
+        System.out.println(sb);
+        
         final Amigo neymar =
-                new Amigo("Neymar");
+                new Amigo("Neymar", "");
         final Amigo ganso =
-                new Amigo("Ganso");
+                new Amigo("Ganso", "");
         new Thread(new Runnable() {
             public void run() { neymar.passar(ganso); }
         }).start();
-        new Thread(new Runnable() {
+        /*new Thread(new Runnable() {
             public void run() { ganso.passar(neymar); }
-        }).start();
+        }).start();*/
     }
 }
